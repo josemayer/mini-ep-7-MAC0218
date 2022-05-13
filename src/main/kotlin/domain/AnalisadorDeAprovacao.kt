f33fb02deda56b761f6dd0a020c2168a621b1e80
José Lucas Silva Mayer <josemayer@usp.br>
2022-05-12 23:31:04 -0300

    Implement final grade calculus function
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,11 +1,23 @@
 package domain
 
 import domain.criterios.CriterioDeAprovacao
+import domain.Boletim
+import domain.BoletimFechado
 
 class AnalisadorDeAprovacao {
 	private lateinit var criterioDoAnalisador: CriterioDeAprovacao
 
 	fun defineCriterio(criterio: CriterioDeAprovacao) {
 		this.criterioDoAnalisador = criterio
+	}
+
+	fun fechaBoletim(boletim: Boletim): BoletimFechado {
+		var boletimFechado = BoletimFechado(
+			boletim.mediaEPs,
+			boletim.mediaMiniEPs,
+			this.criterioDoAnalisador.mediaFinal(boletim),
+			this.criterioDoAnalisador.estaAprovado(boletim)
+		)
+		return boletimFechado
 	}
 }
